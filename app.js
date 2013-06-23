b110980eeba74b02d7b6596106c04a0110f28eb8
Samat K Jain <samat@samat.org>
2013-06-22 23:30:04 -0600

    Upload to S3 (maybe)

--- app.js
+++ app.js
@@ -47,6 +47,12 @@ app.get('/users', user.list);
 app.get('/add',add.add_your_drink);
 app.post('/add', function(req,res){
     console.log(req.files)
+	var fileName = path.basename(req.files.path);
+	knox.putFile(fileName, req.files.path, function (err, res) {
+		if (err)
+			console.log(err);
+		res.end()
+	});
 })
 
 http.createServer(app).listen(app.get('port'), function(){
